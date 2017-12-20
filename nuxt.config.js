eee5045af55aa7e7869bb1a62fdd8ff05f949578
Duard <duardharmse@gmail.com>
2017-12-20 18:25:53 +0200

    Vuetify implementation

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,13 +6,18 @@ module.exports = {
     title: 'dos-2-planner',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
+  plugins: ['~/plugins/vuetify.js'],
+  css: [
+    { src: '~/assets/style/app.styl', lang: 'styl' }
+  ],
   /*
   ** Customize the progress bar color
   */
