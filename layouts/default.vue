16396693ee9b0ecb742587556ea35965231d9f36
Duard <duardharmse@gmail.com>
2017-12-21 13:44:25 +0200

    Tabs layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,57 @@
 <template>
   <v-app>
-    <v-toolbar>
-      <v-toolbar-title>DOS 2 Planner</v-toolbar-title>
-    </v-toolbar>
-    <v-content>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-content>
-    <v-footer app></v-footer>
+    <v-tabs v-model="active" centered>
+      <v-toolbar class="primary elevation-4 mb-3" dark>
+        <v-toolbar-title>
+          <input v-model="name" class="inline-input" style="width: 360px;">
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon @click="decLevel()">
+          <v-icon>remove</v-icon>
+        </v-btn>
+        <input v-model.number.lazy="level" @change="changeLevel()" class="text-xs-center inline-input">
+        <v-btn icon @click="incLevel()">
+          <v-icon>add</v-icon>
+        </v-btn>
+        <v-tabs-bar slot="extension" class="primary" dark>
+          <v-tabs-item href="#attributes" ripple>Attributes</v-tabs-item>
+          <v-tabs-item href="#combat" ripple>Combat</v-tabs-item>
+          <v-tabs-item href="#civil" ripple>Civil</v-tabs-item>
+          <v-tabs-item href="#talents" ripple>Talents</v-tabs-item>
+          <v-tabs-slider></v-tabs-slider>
+        </v-tabs-bar>
+      </v-toolbar>
+      <v-tabs-items>
+
+      </v-tabs-items>
+    </v-tabs>
   </v-app>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      active: null,
+      level: 1,
+      name: 'Tenak'
+    }
+  },
+  methods: {
+    incLevel () {
+      this.level++
+    },
+    decLevel () {
+      if (this.level > 1) {
+        this.level--
+        // this.resetAttributes();
+      }
+    },
+    changeLevel () {
+      if (this.level < 1) {
+        this.level = 1
+      }
+    }
+  }
+}
+</script>
